9ab48d7043f9aeb5864ba3c8b79d23e5b958e8f1
phly6 <gixxer6@gmail.com>
2025-03-02 15:03:46 -0500

    Restored and running.

--- SportsPro/obj/Debug/net9.0/SportsPro.AssemblyInfo.cs
+++ SportsPro/obj/Debug/net9.0/SportsPro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c16eda13bddb342aa57f0b33bb4e9a41dd17dd4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
