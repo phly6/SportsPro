0bb12f39bb217d891f77e7ab1dcf84ef91756b53
phly6 <gixxer6@gmail.com>
2025-03-04 13:18:30 -0500

    Updated 'Add Customer' layout

--- SportsPro/obj/Debug/net9.0/SportsPro.AssemblyInfo.cs
+++ SportsPro/obj/Debug/net9.0/SportsPro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9340cd392a83fbf83ebef6ef7ea14965202c5b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8e758942962dd923feff623bdc5ec6dd42f5013")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
