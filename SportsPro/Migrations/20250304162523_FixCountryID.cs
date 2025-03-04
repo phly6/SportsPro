d8e758942962dd923feff623bdc5ec6dd42f5013
phly6 <gixxer6@gmail.com>
2025-03-04 12:16:07 -0500

    Updated some visuals

---
+++ SportsPro/Migrations/20250304162523_FixCountryID.cs
@@ -0,0 +1,509 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SportsPro.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCountryID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Countries_CountryID",
+                table: "Customers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_CountryID",
+                table: "Customers");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "AE");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "AT");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "AU");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "BE");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "BR");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "CA");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "CH");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "CN");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "DK");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "ES");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "FI");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "FR");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "GB");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "GL");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "GR");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "HK");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "IE");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "IL");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "IN");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "IS");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "IT");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "JP");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "LR");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "MX");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "MY");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "NG");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "NL");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "NZ");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "PH");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "PR");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "PT");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "QA");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "SE");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "SG");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "TH");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "TR");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "UA");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "VN");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "ZW");
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 1004);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 1006);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 1008);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 1012);
+
+            migrationBuilder.DeleteData(
+                table: "Incidents",
+                keyColumn: "IncidentID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Incidents",
+                keyColumn: "IncidentID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Incidents",
+                keyColumn: "IncidentID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Incidents",
+                keyColumn: "IncidentID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Technicians",
+                keyColumn: "TechnicianID",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Technicians",
+                keyColumn: "TechnicianID",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 1002);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 1010);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 1015);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Technicians",
+                keyColumn: "TechnicianID",
+                keyValue: -1);
+
+            migrationBuilder.DeleteData(
+                table: "Technicians",
+                keyColumn: "TechnicianID",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Technicians",
+                keyColumn: "TechnicianID",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Technicians",
+                keyColumn: "TechnicianID",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "US");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CountryID",
+                table: "Customers",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryID1",
+                table: "Customers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_CountryID1",
+                table: "Customers",
+                column: "CountryID1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Countries_CountryID1",
+                table: "Customers",
+                column: "CountryID1",
+                principalTable: "Countries",
+                principalColumn: "CountryID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Countries_CountryID1",
+                table: "Customers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_CountryID1",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "CountryID1",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CountryID",
+                table: "Customers",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "CountryID", "Name" },
+                values: new object[,]
+                {
+                    { "AE", "United Arab Emirates" },
+                    { "AT", "Austria" },
+                    { "AU", "Australia" },
+                    { "BE", "Belgium" },
+                    { "BR", "Brazil" },
+                    { "CA", "Canada" },
+                    { "CH", "Switzerland" },
+                    { "CN", "China" },
+                    { "DK", "Denmark" },
+                    { "ES", "Spain" },
+                    { "FI", "Finland" },
+                    { "FR", "France" },
+                    { "GB", "United Kingdom" },
+                    { "GL", "Greenland" },
+                    { "GR", "Greece" },
+                    { "HK", "Hong Kong" },
+                    { "IE", "Ireland" },
+                    { "IL", "Israel" },
+                    { "IN", "India" },
+                    { "IS", "Iceland" },
+                    { "IT", "Italy" },
+                    { "JP", "Japan" },
+                    { "LR", "Liberia" },
+                    { "MX", "Mexico" },
+                    { "MY", "Malaysia" },
+                    { "NG", "Nigeria" },
+                    { "NL", "Netherlands" },
+                    { "NZ", "New Zealand" },
+                    { "PH", "Philippines" },
+                    { "PR", "Puerto Rico" },
+                    { "PT", "Portugal" },
+                    { "QA", "Qatar" },
+                    { "SE", "Sweden" },
+                    { "SG", "Singapore" },
+                    { "TH", "Thailand" },
+                    { "TR", "Turkey" },
+                    { "UA", "Ukraine" },
+                    { "US", "United States" },
+                    { "VN", "Vietnam" },
+                    { "ZW", "Zimbabwe" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductID", "Name", "ProductCode", "ReleaseDate", "YearlyPrice" },
+                values: new object[,]
+                {
+                    { 1, "Draft Manager 1.0", "DRAFT10", new DateTime(2017, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 4.99m },
+                    { 2, "Draft Manager 2.0", "DRAFT20", new DateTime(2019, 7, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), 5.99m },
+                    { 3, "League Scheduler 1.0", "LEAG10", new DateTime(2016, 5, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 4.99m },
+                    { 4, "League Scheduler Deluxe 1.0", "LEAGD10", new DateTime(2016, 8, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 7.99m },
+                    { 5, "Team Manager 1.0", "TEAM10", new DateTime(2017, 5, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 4.99m },
+                    { 6, "Tournament Master 1.0", "TRNY10", new DateTime(2015, 12, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 4.99m },
+                    { 7, "Tournament Master 2.0", "TRNY20", new DateTime(2018, 2, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), 5.99m }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Technicians",
+                columns: new[] { "TechnicianID", "Email", "Name", "Phone" },
+                values: new object[,]
+                {
+                    { -1, "", "Not assigned", "" },
+                    { 11, "alison@sportsprosoftware.com", "Alison Diaz", "800-555-0443" },
+                    { 12, "jason@sportsprosoftware.com", "Jason Lee", "800-555-0444" },
+                    { 13, "awilson@sportsprosoftware.com", "Andrew Wilson", "800-555-0449" },
+                    { 14, "gunter@sportsprosoftware.com", "Gunter Wendt", "800-555-0400" },
+                    { 15, "gfiori@sportsprosoftware.com", "Gina Fiori", "800-555-0459" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerID", "Address", "City", "CountryID", "Email", "FirstName", "LastName", "Phone", "PostalCode", "State" },
+                values: new object[,]
+                {
+                    { 1002, "PO Box 96621", "Washington", "US", "ania@mma.nidc.com", "Ania", "Irvin", "(301) 555-8950", "20090", "DC" },
+                    { 1004, "1990 Westwood Blvd Ste 260", "Los Angeles", "US", "kenzie@mma.jobtrak.com", "Kenzie", "Quinn", "(800) 555-8725", "90025", "CA" },
+                    { 1006, "3255 Ramos Cir", "Sacramento", "US", "amauro@yahoo.org", "Anton", "Mauro", "(916) 555-6670", "95827", "CA" },
+                    { 1008, "Box 52001", "San Francisco", "US", "kanthoni@pge.com", "Kaitlyn", "Anthoni", "(800) 555-6081", "94152", "CA" },
+                    { 1010, "PO Box 2069", "Fresno", "US", "kmayte@fresno.ca.gov", "Kendall", "Mayte", "(559) 555-9999", "93718", "CA" },
+                    { 1012, "4420 N. First Street, Suite 108", "Fresno", "US", "marvin@expedata.com", "Marvin", "Quintin", "(559) 555-9586", "93726", "CA" },
+                    { 1015, "27371 Valderas", "Mission Viejo", "US", "", "Gonzalo", "Keeton", "(214) 555-3647", "92691", "CA" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Incidents",
+                columns: new[] { "IncidentID", "CustomerID", "DateClosed", "DateOpened", "Description", "ProductID", "TechnicianID", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1010, new DateTime(2020, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2020, 1, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), "Media appears to be bad.", 1, 11, "Could not install" },
+                    { 2, 1002, null, new DateTime(2020, 1, 9, 0, 0, 0, 0, DateTimeKind.Unspecified), "Received error message 415 while trying to import data from previous version.", 4, 14, "Error importing data" },
+                    { 3, 1015, new DateTime(2020, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2020, 1, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), "Setup failed with code 104.", 6, 15, "Could not install" },
+                    { 4, 1010, null, new DateTime(2020, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "Program fails with error code 510, unable to open database.", 3, -1, "Error launching program" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_CountryID",
+                table: "Customers",
+                column: "CountryID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Countries_CountryID",
+                table: "Customers",
+                column: "CountryID",
+                principalTable: "Countries",
+                principalColumn: "CountryID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- SportsPro/obj/Debug/net9.0/SportsPro.AssemblyInfo.cs
+++ SportsPro/obj/Debug/net9.0/SportsPro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a429d957c0f7fa0124434b53889a030f10bbac44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9340cd392a83fbf83ebef6ef7ea14965202c5b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
